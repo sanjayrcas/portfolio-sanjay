42dfcfacb50be080db02d7f268e39169c0ab62aa
SanjayRcas <sanjaygrcas@gmail.com>
2025-06-24 23:09:21 +0530

    miner update on header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,27 +1,28 @@
 import React, { useState } from 'react';
 import { Bars3Icon } from '@heroicons/react/24/solid';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
     const [toggleMenu,setToggleMenu] = useState(false);
     return (
         <header className='flex justify-between px-5 py-2 bg-primary'>
-            <a href="#" className='font-bold text-black'>Sanjay G</a>
+            <Link to="#" className='font-bold text-black'>Sanjay G</Link>
             <nav className='hidden md:block'>
                 <ul className='flex text-white'>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#projects">Projects</a></li>
-                    <li><a href="#resume">Resume</a></li>
-                    <li><a href="#contact">Contact</a></li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="#about">About</Link></li>
+                    <li><Link to="#projects">Projects</Link></li>
+                    <li><Link to="#resume">Resume</Link></li>
+                    <li><Link to="#contact">Contact</Link></li>
                 </ul>
             </nav>
             {toggleMenu && <nav className='block md:hidden'>
                 <ul onClick={()=>setToggleMenu(!toggleMenu)} className='flex flex-col text-white mobile-nav'>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#projects">Projects</a></li>
-                    <li><a href="#resume">Resume</a></li>
-                    <li><a href="#contact">Contact</a></li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="#about">About</Link></li>
+                    <li><Link to="#projects">Projects</Link></li>
+                    <li><Link to="#resume">Resume</Link></li>
+                    <li><Link to="#contact">Contact</Link></li>
                 </ul>
             </nav>}
             <button className='block md:hidden' onClick={()=>setToggleMenu(!toggleMenu)}><Bars3Icon className='text-white h-5' /></button>
