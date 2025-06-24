0b10478816497f16ac06a560153a40a2fb8ed412
SanjayRcas <sanjaygrcas@gmail.com>
2025-06-24 23:36:07 +0530

    changes-0.3

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import { Bars3Icon } from '@heroicons/react/24/solid';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
     const [toggleMenu,setToggleMenu] = useState(false);
     return (
         <header className='flex justify-between px-5 py-2 bg-primary'>
-            <a href="#" className='font-bold text-black'>Sanjay G</a>
+            <Link to="/" className='font-bold text-black'>Sanjay G</Link>
             <nav className='hidden md:block'>
                 <ul className='flex text-white'>
                     <li><a href="/">Home</a></li>
